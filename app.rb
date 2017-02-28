1256306831df9f7abd0c79d8da8690c7152fa7bb
Alice Armstrong <alicearmstrong@Alices-MacBook-Pro.local>
2017-02-28 15:35:08 +0000

    Sinatra Posted Params. Post done.

--- app.rb
+++ app.rb
@@ -18,9 +18,16 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @cat_name = params[:name]
   @cat_age = params[:age]
   erb(:index)
+end
+
+get '/naming-cat' do
+  p params
+  @cat_name = params[:name]
+  @cat_age = params[:age]
+  erb(:catname)
 end
