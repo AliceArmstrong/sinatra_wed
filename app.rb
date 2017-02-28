dc339bc86ad6f15dc0057d06483e69f7bde2715a
joemaidman <joemaidman@gmail.com>
2017-02-28 12:05:55 +0000

    Moved HTML to the ERB file

--- app.rb
+++ app.rb
@@ -14,6 +14,5 @@ get '/bye' do
 end
 
 get '/cat' do
-'<img style="border: 2px dotted red" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png"/>'
-
+  erb(:index)
 end
